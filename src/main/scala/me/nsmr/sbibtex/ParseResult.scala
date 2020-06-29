6ae38b090fbdc7b0430439168fe5a962f58f1eaa
nil <lue.gsk.2012@gmail.com>
2020-06-29 15:49:53 +0900

    Add some methods to ParseResult

--- src/main/scala/me/nsmr/sbibtex/ParseResult.scala
+++ src/main/scala/me/nsmr/sbibtex/ParseResult.scala
@@ -4,15 +4,29 @@ sealed abstract class ParseResult[+T] {
   def source: String
 
   def result: T
+
+  def isSuccess: Boolean
+
+  def isFailure: Boolean = !this.isSuccess
+
+  def toOption: Option[T] = if(this.isSuccess) Option(result) else None
 }
 
 object ParseResult {
 
-  case class Success[+T](result: T, source: String) extends ParseResult[T]
+  case class Success[+T](result: T, source: String) extends ParseResult[T] {
+
+    override def isSuccess: Boolean = true
+
+  }
 
   case class Failure(source: String) extends ParseResult[Nothing] {
+
+    override def isSuccess: Boolean = false
+
     override def result = throw new UnsupportedOperationException("Failure does not have any result...")
-  }
+
+   }
 
 }
 
